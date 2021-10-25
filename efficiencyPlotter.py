e625ca2d018ac390baa097fce9bffdb75a6ae4b9
John <john.rotter@ufl.edu>
2021-10-24 21:32:16 -0500

    Updated efficiency plotter

--- efficiencyPlotter.py
+++ efficiencyPlotter.py
@@ -8,24 +8,34 @@ import mplhep as hep
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
+import scipy
 import awkward
+import sys
 
 if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] outputDir inputFiles")
     parser.add_option("--emtf-mode", dest="emtf_mode", type="long",
                      help="EMTF Track Mode", default=15)
-    parser.add_option("--eta-min", dest="eta_min", type="float",
-                     help="Minimum Eta", default=1.25)
-    parser.add_option("--eta-max", dest="eta_max", type="float",
-                     help="Maximum Eta", default=2.5)
-    parser.add_option("--pt-cut", dest="pt_cut", type="float",
-                     help="EMTF pt Cut (GeV)", default=22)
+    parser.add_option("--eta-mins", dest="eta_mins", type="string",
+                     help="Array of Minimum Eta (Must be same length as --eta-maxs)", default="[1.25]")
+    parser.add_option("--eta-maxs", dest="eta_maxs", type="string",
+                     help="Array of Maximum Eta (Must be same length as --eta-mins)", default="[2.5]")
+    parser.add_option("--pt-cuts", dest="pt_cuts", type="string",
+                     help="Array of EMTF pt Cuts (GeV)", default="[22]")
     parser.add_option("--nEvents", dest="num_events", type="int",
                      help="Number of Events", default=-1)
     parser.add_option("-v","--verbose", dest="verbose",
                      help="Print extra debug info", default=False)
     options, args = parser.parse_args()
+    
+    options.pt_cuts = [float(pt) for pt in options.pt_cuts[1:-1].split(',')]
+    options.eta_mins = [float(eta) for eta in options.eta_mins[1:-1].split(',')]
+    options.eta_maxs = [float(eta) for eta in options.eta_maxs[1:-1].split(',')]
+
+    if(len(options.eta_mins) != len(options.eta_maxs)):
+        parser.print_help()
+        sys.exit(1)
 
     if(options.verbose):
         print("#######################################################")
@@ -49,38 +59,93 @@ if __name__ == "__main__":
 
     if(options.verbose):
         print("\nCreating ETA Mask and PT_cut MASK\n")
-        print("Applying:\n   " + str(options.eta_min) + " < eta < " + str(options.eta_max))
-        print("   " + str(options.pt_cut) + "GeV < pT")
-    
-    unbinned_eta_mask = ((options.eta_min < abs(unbinned_GEN_eta)) & (options.eta_max > abs(unbinned_GEN_eta)) )
-
-    unbinned_BDT_pt = unbinned_BDT_pt[unbinned_eta_mask]
-    unbinned_GEN_pt = unbinned_GEN_pt[unbinned_eta_mask]
-
-    unbinned_BDT_pt_mask = (options.pt_cut < unbinned_BDT_pt)
-    unbinned_GEN_pt_pass = unbinned_GEN_pt[unbinned_BDT_pt_mask]
+        print("Applying:\n   " + str(options.eta_mins) + " < eta < " + str(options.eta_maxs))
+        print("   " + str(options.pt_cuts) + "GeV < pT")
     
     import efficiencyPlotter
-    efficiencyPlotter.makeEfficiencyPlot(unbinned_GEN_pt_pass, unbinned_GEN_pt,
-                                              "EMTF BDT Efficiency| mode:" + str(options.emtf_mode)
-                                              + " | " + str(options.eta_min) + " < $\eta$ < " + str(options.eta_max)
-                                              + " | " + str(options.pt_cut) + "GeV < $p_T$", options.verbose)
+    from matplotlib.backends.backend_pdf import PdfPages
 
     if(options.verbose):
-        print("\nDisplaying Plot\n")
+        print("\nOpening PDF:" + args[0] + "/plots.pdf" + "\n")
+    pp = PdfPages(args[0] + "/plots.pdf")
 
-    plt.show()
+    pt_cuts = options.pt_cuts
+    eta_mins = options.eta_mins
+    eta_maxs = options.eta_maxs
+    
+    for pt_cut in pt_cuts:
+        for i in range(0, len(eta_mins)):
+            if(options.verbose):
+                print("##############################################")
+            unbinned_GEN_pt_eta_masked, unbinned_GEN_pt_pass_eta_masked = efficiencyPlotter.maskEvents(unbinned_GEN_pt, unbinned_BDT_pt, unbinned_GEN_eta, eta_mins[i], eta_maxs[i], pt_cut, options.verbose)
+            fig = efficiencyPlotter.makeEfficiencyPlot(unbinned_GEN_pt_pass_eta_masked, unbinned_GEN_pt_eta_masked,
+                                              "EMTF BDT Efficiency", "mode:" + str(options.emtf_mode)
+                                              + "\n" + str(eta_mins[i]) + " < $\eta$ < " + str(eta_maxs[i])
+                                              + "\n" + str(pt_cut) + "GeV < $p_T$", options.verbose)
+            fig.set_size_inches(6, 4.5)
+            pp.savefig(fig)
+    if(options.verbose):
+        print("\nClosing PDF\n")
+    pp.close()
+    #plt.show()
+
+def maskEvents(unbinned_GEN_pt, unbinned_BDT_pt, unbinned_GEN_eta, eta_min, eta_max, pt_cut, verbose):
+    if(verbose):
+        print("\nCreating ETA Mask and PT_cut MASK\n")
+        print("Applying:\n   " + str(eta_min) + " < eta < " + str(eta_max))
+        print("   " + str(pt_cut) + "GeV < pT")
+    unbinned_eta_mask = ((eta_min < abs(unbinned_GEN_eta)) & (eta_max > abs(unbinned_GEN_eta)))
+
+    unbinned_BDT_pt_eta_masked = unbinned_BDT_pt[unbinned_eta_mask]
+    unbinned_GEN_pt_eta_masked = unbinned_GEN_pt[unbinned_eta_mask]
+
+    unbinned_BDT_pt_mask_eta_masked = (pt_cut < unbinned_BDT_pt_eta_masked)
+    unbinned_GEN_pt_pass_eta_masked = unbinned_GEN_pt_eta_masked[unbinned_BDT_pt_mask_eta_masked]
+    return unbinned_GEN_pt_eta_masked, unbinned_GEN_pt_pass_eta_masked
 
 def getEfficiciencyHist(num_binned, den_binned):
     efficiency_binned = np.array([])
+    efficiency_binned_err = [np.array([]), np.array([])]
+    alpha = .32
     for i in range(0, len(den_binned)):
         if(den_binned[i] == 0):
             efficiency_binned = np.append(efficiency_binned, 0)
+            efficiency_binned_err[0] = np.append(efficiency_binned_err[0], [0])
+            efficiency_binned_err[1] = np.append(efficiency_binned_err[1], [1])
             continue
         efficiency_binned = np.append(efficiency_binned, [num_binned[i]/den_binned[i]])
-    return efficiency_binned
+        efficiency_binned_err[0] = np.append(efficiency_binned_err[0], [(efficiency_binned[i] - scipy.stats.beta.ppf(alpha/2, num_binned[i], den_binned[i]-num_binned[i]+1))/efficiency_binned[i]])
+        efficiency_binned_err[1] = np.append(efficiency_binned_err[1], [(scipy.stats.beta.ppf(1 - alpha/2, num_binned[i]+1, den_binned[i]-num_binned[i]) - efficiency_binned[i])/efficiency_binned[i]])# - efficiency_binned[i]])
+    return efficiency_binned, efficiency_binned_err
 
-def makeEfficiencyPlot(num_unbinned, den_unbinned, title, verbose=False):
+def makeEfficiencyPlot(num_unbinned, den_unbinned, title, textStr, verbose=False):
+
+    if(verbose):
+        print("\nInitializing Figures and Binning Histograms")
+
+    #plt.style.use(hep.style.CMS)
+
+    den_binned, den_bins = np.histogram(den_unbinned, 250, (0,1000))
+    num_binned, num_bins = np.histogram(num_unbinned, 250, (0,1000))
+
+    if(verbose):
+        print("Generating Efficiency Plot")
+    efficiency_binned, efficiency_binned_err = getEfficiciencyHist(num_binned, den_binned)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.errorbar(den_bins[0:-1], efficiency_binned, yerr=efficiency_binned_err, xerr=None, capsize=3, linestyle="", marker=".")#efficiency_binned_err
+    ax.set_ylabel("Efficiency")
+    ax.set_xlabel("$p_T$(GeV)")
+    ax.set_title(title)
+    props = dict(boxstyle='square', facecolor='white', alpha=1.0)
+    # place a text box in upper left in axes coords
+    ax.text(0.90, 0.05, textStr, transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=props)
+
+    if(verbose):
+        print("Finished Creating Figures\n")
+    return fig
+
+def makeEfficiencyPlotWithHistograms(num_unbinned, den_unbinned, title, verbose=False):
 
     if(verbose):
         print("\nInitializing Figures and Binning Histograms")
@@ -99,13 +164,13 @@ def makeEfficiencyPlot(num_unbinned, den_unbinned, title, verbose=False):
 
     if(verbose):
         print("Generating Efficiency Plot")
-
-    efficiency_binned = getEfficiciencyHist(num_binned, den_binned)
+    efficiency_binned, efficiency_binned_err = getEfficiciencyHist(num_binned, den_binned)
     f2_ax2 = fig2.add_subplot(spec2[2, :])
-    f2_ax2.plot(den_bins[0:-1], efficiency_binned)
+  
+    #f2_ax2.plot(den_bins[0:-1], efficiency_binned)
+    f2_ax2.errorbar(den_bins[0:-1], efficiency_binned, yerr=efficiency_binned_err, xerr=None, capsize=3, linestyle="", marker=".")#efficiency_binned_err
     f2_ax2.set_ylabel("Efficiency")
     f2_ax2.set_xlabel("$p_T$(GeV)")
-
     if(verbose):
         print("Finished Creating Figures\n")
     
