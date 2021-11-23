e3704c421c8fcc1ea13daa0f6ac728eedf5f0bbb
John <john.rotter@ufl.edu>
2021-11-23 10:39:15 -0600

    Fixed Bug, Added Stacked Plots

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,15 +1,17 @@
 import numpy as np
 
 def scaleBDTFunction(unbinned_BDT_pT, pt_scaling_A, pt_scaling_B):
-
     pt_min = 20
-    unbinned_BDT_pT_scaled = np.where(unbinned_BDT_pT > pt_min, unbinned_BDT_pT, (pt_scaling_A * unbinned_BDT_pT)/(1 - (pt_scaling_B * unbinned_BDT_pT)))
-    unbinned_BDT_pT_scaled = np.where(unbinned_BDT_pT <= pt_min, unbinned_BDT_pT_scaled, (pt_scaling_A * unbinned_BDT_pT)/(1 - (pt_scaling_B * pt_min)))
+    unbinned_BDT_pT_scaled = np.where(unbinned_BDT_pT > pt_min, (pt_scaling_A * unbinned_BDT_pT)/(1 - (pt_scaling_B * pt_min)), (pt_scaling_A * unbinned_BDT_pT)/(1 - (pt_scaling_B * unbinned_BDT_pT)))
+
+    for x in range(0,100):
+        print(unbinned_BDT_pT_scaled[x], unbinned_BDT_pT[x], pt_scaling_A, pt_scaling_B, unbinned_BDT_pT[x] >= pt_min,(pt_scaling_A * unbinned_BDT_pT[x])/(1 - (pt_scaling_B * pt_min)), (pt_scaling_A * unbinned_BDT_pT[x])/(1 - (pt_scaling_B * unbinned_BDT_pT[x])))
 
     return unbinned_BDT_pT_scaled
 
 def scaleBDTPtRun2(unbinned_BDT_pT):
-    return scaleBDTFunction(unbinned_BDT_pT, 1.2, 0.015)
+     return scaleBDTFunction(unbinned_BDT_pT, 1.2, .015)
+#    return scaleBDTFunction(unbinned_BDT_pT, 1.15, .009)
 
 def scaleBDTPtRun3(unbinned_BDT_pT):
     return scaleBDTFunction(unbinned_BDT_pT, 1.3, 0.004)
