7ecec2dc253f6bf6bcfba853bc05e31e6111c1d0
tbcarnahan <tbc5@rice.edu>
2021-12-01 14:25:36 -0600

    Working Gaussian

--- resolutionPlotter.py
+++ resolutionPlotter.py
@@ -7,10 +7,13 @@ import helpers.fileHelper as fileHelper
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+#import matplotlib.mlab as mlab
 import numpy as np
 
 import scipy
 from scipy.stats import norm
+from scipy.optimize import curve_fit #for Gaussian
+
 import awkward
 import sys
 import os
@@ -91,12 +94,19 @@ if __name__ == "__main__":
         for i in range(0, len(eta_mins)):
             if(options.verbose):
                 print("###################   New Cuts   ###################")
+#Future iteration: apply GEN_PT Mask for low, med, high pT muons                                                                 
+            unbinned_EVT_data_GEN_pT_pass = helper.applyMaskToEVTData(                                                            
+                                            unbinned_EVT_data,                                                                   
+                                            ["GEN_pt","BDT_pt", "GEN_eta", "TRK_hit_ids"],                                                  
+                                            (50 > unbinned_EVT_data["GEN_pt"]),                                              
+                                            "GEN_PT CUT: " + str(pt_cut) + " < pT", options.verbose)                                            
+
 
             # Apply ETA Mask
             unbinned_EVT_data_eta_masked = helper.applyMaskToEVTData(
-                                            unbinned_EVT_data,
+                                            unbinned_EVT_data_GEN_pT_pass,
                                             ["GEN_pt", "BDT_pt", "GEN_eta", "TRK_hit_ids"], 
-                                            ((eta_mins[i] < abs(unbinned_EVT_data["GEN_eta"])) & (eta_maxs[i] > abs(unbinned_EVT_data["GEN_eta"]))),
+                                            ((eta_mins[i] < abs(unbinned_EVT_data_GEN_pT_pass["GEN_eta"])) & (eta_maxs[i] > abs(unbinned_EVT_data_GEN_pT_pass["GEN_eta"]))),
                                             "ETA CUT: " + str(eta_mins[i]) + " < eta < " + str(eta_maxs[i]), options.verbose)
 
             #Future iteration: apply GEN_PT Mask for low, med, high pT muons
@@ -109,11 +119,11 @@ if __name__ == "__main__":
 
             # Generate resolution with eta input plot
             res_plot = resolutionPlotter.makeResolutionPlot(unbinned_EVT_data_eta_masked["GEN_pt"], unbinned_EVT_data_eta_masked["BDT_pt"],
-                                               "EMTF BDT Resolution", "mode: " + str(options.emtf_mode)
+                                               "EMTF BDT Resolution \n Emulation in CMSSW_12_1_0_pre3", "mode: " + str(options.emtf_mode)
                                               + "\n" + str(eta_mins[i]) + " < $\eta$ < " + str(eta_maxs[i])
                                               + "\n $p_T$ > " + str(pt_cut) + "GeV"
                                               + "\n" + "$N_{events}$: "+str(len(unbinned_EVT_data_eta_masked["GEN_pt"])), options.verbose)
-
+            
             # Increase size to square 6in x 6in on PDF
             res_plot.set_size_inches(6,6)
 
@@ -215,12 +225,13 @@ def makeResolutionPlot(unbinned_GEN_pt, unbinned_BDT_pt, title, textStr, verbose
 
     if(verbose):
         print("\nInitializing Figures and Binning Pt Histograms")
-
+    for x in range(0,100):
+        print(unbinned_GEN_pt[x], unbinned_BDT_pt[x], 1-unbinned_GEN_pt[x]/unbinned_BDT_pt[x])
     # Define resolution (unbinned)
     res_unbinned = 1 - np.divide(unbinned_GEN_pt, unbinned_BDT_pt) #inverse pT relationship proportional to bending res
-
+    res_unbinned_masked = res_unbinned[((res_unbinned < .4) & (res_unbinned > -.4))]
     # Initializing bins and binning histograms from unbinned entries
-    res_binned, res_bins = np.histogram(res_unbinned, 500, (-256,256), density=True) #res_unbinned, bins=50, range (min - max pT), density=True means normalize data at bins (np.histogram(res_unbinned,500,(-256,256),density=True))
+    res_binned, res_bins = np.histogram(res_unbinned, 51, (-2,2), density=True) #res_unbinned, bins=50, range (min - max pT), density=True means normalize data at bins (np.histogram(res_unbinned,500,(-256,256),density=True))
                                                             
     if(verbose):
         print("Generating Resolution Plot")
@@ -232,52 +243,87 @@ def makeResolutionPlot(unbinned_GEN_pt, unbinned_BDT_pt, title, textStr, verbose
     ax.errorbar([res_bins[i]+(res_bins[i+1]-res_bins[i])/2 for i in range(0, len(res_bins)-1)],
                     res_binned, xerr=[(res_bins[i+1] - res_bins[i])/2 for i in range(0, len(res_bins)-1)], #xerr = 10.24
                     linestyle="", marker=".", markersize=3, elinewidth = .5)
-   
-# Define Gaussian distribution:
-    """
-# Attempt 1:
-    x_axis = np.arange(-50,50,0.1) #need to define the variable here
-    mean = statistics.mean(x_axis)
-    sd = statistics.stdev(x_axis)
-    ax.plot(x_axis, norm.pdf(x_axis, mean, sd))
-    ax.show()
-    
-    """
+
+#Gaussian Attempts:
+
+#++++++++++++++++++++++++++++++++++++ (Lessons learned)
+
+#+++++++++++++++++++++++++++++ mlab doesn't work as well as scipy.stats.norm.pdf
+   #best fit of data
+    #(mu,sigma) = norm.fit(res_unbinned)
+   #histo
+    #n, bins, patches = plt.hist(res_unbinned, 50, normed=1, facecolor='blue', alpha=0.7)
+   #best fit line
+    #y = mlab.normpdf(50, mu, sigma)
+    #l = plt.plot(50, y, 'r--', linewidth=2)
+    #plt.show()
+#+++++++++++++++++++++++++++++++++++ Need to define Gaussian distribution:
+
+#
+    #x_axis = np.arange(-50,50,0.1) #need to define the variable here
+    #mean = statistics.mean(x_axis)
+    #sd = statistics.stdev(x_axis)
+    #ax.plot(x_axis, norm.pdf(x_axis, mean, sd))
+    #ax.show()
+
+#+++++++++++++++++++++++++++++++ Make sure indentations match above functions
+#Keep for future: This one actually gives us something but is matching the wrong data set...
 
     #Fit a normal distribution to the res data:
-    mu, sigma = scipy.stats.norm.fit(res_unbinned)
+    mu, std = norm.fit(res_unbinned_masked)
+    #print(np.mean(res_unbinned), np.std(res_unbinned))
+    #print(norm.fit(res_unbinned))
     #fit_line = scipy.stats.norm.pdf(50, mu, sigma)#print(mu,sigma)
     #plt.plot(50, fit_line)
-    print(mu,sigma)
+    print(mu,std)
 
-    plt.hist(res_unbinned, bins=500, density=True, alpha=0.6, color='b')
+    #plt.hist(res_unbinned, bins=500, density=True, alpha=0.6, color='b')
     #xmin, xmax = plt.xlim()
-    x = np.linspace(-256, 256) #xmin, xmax, num:  int, optional (# samples to generate; default 50)
-    p = norm.pdf(x, mu, sigma)
-    plt.plot(x,p) #was ax.plot(x,p)
+    x = np.linspace(-0.4, 0.4)#plot fit #xmin, xmax, num:  int, optional (# samples to generate; default 50)
+    p = norm.pdf(x, mu, std)
+#    p = p*np.max(res_binned)#scale up to peak
+    ax.plot(x,p) #was ax.plot(x,p)
+    
+    
+    #attempt at defining gaussian
+    def Gauss(x, A, B, C):
+        y = A*np.exp(-1*(x-B)**2/C**2)
+        return y
+    parameters, covariance = curve_fit(Gauss, res_bins[0:-1], res_binned)#curve_fit(model f(x), xdata, ydata, p0=None, sigma #add ydata = Num of events
 
+    fit_A = parameters[0]
+    fit_B = parameters[1]
+    fit_C = parameters[2]
+
+    print(fit_A, fit_B, fit_C)
+
+    fit_y = Gauss(res_unbinned, fit_A, fit_B, fit_C)
+    #ax.plot(res_unbinned, fit_y, '-', label='fit')
+
+#++++++++++++++++++++++ python3 tutorial: https://www.geeksforgeeks.org/python-gaussian-fit/
+
+    #Extraneous info addendum for above "Keep for future":
     #title = "Fit results: mu = %.2f, std = %.2f" % (mu,std)
     #plt.title(title)
     #plt.show()
     #mu, sigma = 0, 0.1 #mean and std dev
     #s = np.default_rng().normal(mu, sigma, 1000)
-    """
-    plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2/ (2 * sigma**2) ), linewidth=2, color='r')
-    plt.plot(x, p, 'k', linewidth=2)
-    title = "CMSSW_12_1_0_pre3 IIRC Res Fit results: mu = %.2f, std = %.2f" % (mu, std)
-    plt.title(title)
-    plt.show()
-    """
+    
+    #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2/ (2 * sigma**2) ), linewidth=2, color='r')
+    #plt.plot(x, p, 'k', linewidth=2)
+    #title = "CMSSW_12_1_0_pre3 IIRC Res Fit results: mu = %.2f, std = %.2f" % (mu, std)
+    #plt.title(title)
+    #plt.show()
 
     # Setting labels and plot configs
-    ax.set_ylabel("$N_{events}$") #y-axis = number of events
+    ax.set_ylabel("a.u.") #y-axis = number of events
     ax.set_xlabel("$(1/p_T^{GEN} - 1/p_T^{BDT})/(1/p_T^{GEN})$") #x-axis = binned resolution
     ax.grid(color='lightgray', linestyle='--', linewidth=.25)
     
     # Adding a text box to bottom right
     props = dict(boxstyle='square', facecolor='white', alpha=1.0)
-    ax.text(0.95, 0.05, textStr, transform=ax.transAxes, fontsize=10, verticalalignment='bottom', horizontalalignment='right', bbox=props)
-    ax.set_xlim([-256,256])
+    ax.text(0.95, 0.95, textStr + "\n $\mu$ = " + str(mu) + "\n $\sigma$ = " + str(std), transform=ax.transAxes, fontsize=10, verticalalignment='top', horizontalalignment='right', bbox=props)
+    ax.set_xlim([-2.5,2.5])
     # Setting all font sizes to be small (Less distracting)
     for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
         item.set_fontsize(8)
